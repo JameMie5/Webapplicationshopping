190ef36c88af5dbb6880fa1563bba7140d6219aa
Jame Mie <oosero25@gmail.com>
2023-08-03 23:03:54 +0700

    add project react

---
+++ client/src/components/product/ProductItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+
+class ProductItem extends Component{
+
+    constructor(props){
+    super(props);
+
+
+    }
+
+    render(){
+        const {productName, unitPrice, thumbnail, คำอธิบาย, แคลอรี่} = this.props.product;
+
+        return (
+            <div className="col-md-3 col-sm-6">
+                <img className="img-fluid img-thumbnail" src={thumbnail} /> 
+                <h5 className="mt-2">{productName}</h5>               
+                <p >{คำอธิบาย}</p>
+                <p className="title text-right">{unitPrice} THB </p>
+                <p className="title text-right">{แคลอรี่} Calories </p>
+
+                { this.props.onAddOrder &&
+                <button className="btn btn-block btn-secondary tittle"  value= {unitPrice} onClick={() => this.props.onAddOrder(this.props.product)}>
+                    ซื้อ
+                </button>
+                }
+
+                { (this.props.onDelProduct || this.props.onEditProduct ) && 
+                <button className="btn btn-info col-5 tittle" onClick={() => this.props.onEditProduct(this.props.product)}>  
+                    แก้ไข
+                </button>
+                }
+
+                { (this.props.onDelProduct || this.props.onEditProduct ) &&
+                <button className="btn btn-danger btn-danger col-5 float-right tittle"  value= {unitPrice} onClick={() => this.props.onDelProduct(this.props.product)}>
+                    ลบ
+                </button>
+                }
+                <hr />
+            </div>
+        )
+    }
+}
+export default ProductItem;
---
+++ client/src/components/product/formFields.js
@@ -0,0 +1,8 @@
+
+export const productFormField = [
+    { label: "Product Name", name: "productName", type: "text", required : true},
+    { label: "Unit Price", name: "unitPrice", type: "number", required : true},
+    { label: "Thumbnail", name: "thumbnail", type: "text", required : true},
+    { label : "คำอธิบาย", name : "คำอธิบาย", type : "text", required : true },
+    { label : "แคลอรี่", name : "แคลอรี่", type : "number", required : true }
+];
